0c969ef3833c01c1bc446d5ef76523b2648afa48
auplt <frogyboom@mail.ru>
2022-11-19 23:09:59 +0300

    Initial commit

---
+++ dbconnection.py
@@ -0,0 +1,21 @@
+import psycopg2
+#import sqlite3
+class DbConnection:
+    def __init__(self, config):
+        self.path = config.dbfilepath
+        self.prefix = config.dbtableprefix
+        self.conn = psycopg2.connect(dbname=config.database, user=config.user,password=config.password, host=config.host)
+ #sqlite3.connect(self.path)
+    def __del__(self):
+        if self.conn:
+            self.conn.close()
+    def test(self):
+        cur = self.conn.cursor()
+        cur.execute("CREATE TABLE test(test integer)")
+        cur.execute("INSERT INTO test(test) VALUES(1)")
+        self.conn.commit()
+        cur.execute("SELECT * FROM test")
+        result = cur.fetchall()
+        cur.execute("DROP TABLE test")
+        self.conn.commit()
+        return (result[0][0] == 1)
---
+++ project_config.py
@@ -0,0 +1,18 @@
+import yaml
+
+class ProjectConfig:
+    """Класс считывает базовые настройки из файла config.yaml"""
+    def __init__(self):
+        with open('config.yaml') as f:
+            config = yaml.safe_load(f)
+            self.dbfilepath = config['dbfilepath']
+            self.dbtableprefix = config['dbtableprefix']
+            self.host = config['host']
+            self.password = config['password']
+            self.user = config['user']
+            self.database = config['database']
+        # print(config)
+
+if __name__ == "__main__":
+    x = ProjectConfig()
+    print(x.dbfilepath)
